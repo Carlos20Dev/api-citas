1425b1a1ede517581c01a4beffb09cab19b18100
Carlos20Dev <carlos.tezen.1999@gmail.com>
2024-11-17 23:49:35 -0500

    -- buscar por id_medico en turnos

--- build/controller/reg-turno.controller.js
+++ build/controller/reg-turno.controller.js
@@ -4,7 +4,7 @@ function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.updateRegistroTurno = exports.getRegistroTurnoById = exports.getRegistroTurno = exports.deleteRegistroTurno = exports.createRegistroTurno = void 0;
+exports.updateRegistroTurno = exports.getRegistroTurnoByIdMedico = exports.getRegistroTurnoById = exports.getRegistroTurno = exports.deleteRegistroTurno = exports.createRegistroTurno = void 0;
 var _database = require("../database");
 function _regeneratorRuntime() { "use strict"; /*! regenerator-runtime -- Copyright (c) 2014-present, Facebook, Inc. -- license (MIT): https://github.com/facebook/regenerator/blob/main/LICENSE */ _regeneratorRuntime = function _regeneratorRuntime() { return e; }; var t, e = {}, r = Object.prototype, n = r.hasOwnProperty, o = Object.defineProperty || function (t, e, r) { t[e] = r.value; }, i = "function" == typeof Symbol ? Symbol : {}, a = i.iterator || "@@iterator", c = i.asyncIterator || "@@asyncIterator", u = i.toStringTag || "@@toStringTag"; function define(t, e, r) { return Object.defineProperty(t, e, { value: r, enumerable: !0, configurable: !0, writable: !0 }), t[e]; } try { define({}, ""); } catch (t) { define = function define(t, e, r) { return t[e] = r; }; } function wrap(t, e, r, n) { var i = e && e.prototype instanceof Generator ? e : Generator, a = Object.create(i.prototype), c = new Context(n || []); return o(a, "_invoke", { value: makeInvokeMethod(t, r, c) }), a; } function tryCatch(t, e, r) { try { return { type: "normal", arg: t.call(e, r) }; } catch (t) { return { type: "throw", arg: t }; } } e.wrap = wrap; var h = "suspendedStart", l = "suspendedYield", f = "executing", s = "completed", y = {}; function Generator() {} function GeneratorFunction() {} function GeneratorFunctionPrototype() {} var p = {}; define(p, a, function () { return this; }); var d = Object.getPrototypeOf, v = d && d(d(values([]))); v && v !== r && n.call(v, a) && (p = v); var g = GeneratorFunctionPrototype.prototype = Generator.prototype = Object.create(p); function defineIteratorMethods(t) { ["next", "throw", "return"].forEach(function (e) { define(t, e, function (t) { return this._invoke(e, t); }); }); } function AsyncIterator(t, e) { function invoke(r, o, i, a) { var c = tryCatch(t[r], t, o); if ("throw" !== c.type) { var u = c.arg, h = u.value; return h && "object" == _typeof(h) && n.call(h, "__await") ? e.resolve(h.__await).then(function (t) { invoke("next", t, i, a); }, function (t) { invoke("throw", t, i, a); }) : e.resolve(h).then(function (t) { u.value = t, i(u); }, function (t) { return invoke("throw", t, i, a); }); } a(c.arg); } var r; o(this, "_invoke", { value: function value(t, n) { function callInvokeWithMethodAndArg() { return new e(function (e, r) { invoke(t, n, e, r); }); } return r = r ? r.then(callInvokeWithMethodAndArg, callInvokeWithMethodAndArg) : callInvokeWithMethodAndArg(); } }); } function makeInvokeMethod(e, r, n) { var o = h; return function (i, a) { if (o === f) throw Error("Generator is already running"); if (o === s) { if ("throw" === i) throw a; return { value: t, done: !0 }; } for (n.method = i, n.arg = a;;) { var c = n.delegate; if (c) { var u = maybeInvokeDelegate(c, n); if (u) { if (u === y) continue; return u; } } if ("next" === n.method) n.sent = n._sent = n.arg;else if ("throw" === n.method) { if (o === h) throw o = s, n.arg; n.dispatchException(n.arg); } else "return" === n.method && n.abrupt("return", n.arg); o = f; var p = tryCatch(e, r, n); if ("normal" === p.type) { if (o = n.done ? s : l, p.arg === y) continue; return { value: p.arg, done: n.done }; } "throw" === p.type && (o = s, n.method = "throw", n.arg = p.arg); } }; } function maybeInvokeDelegate(e, r) { var n = r.method, o = e.iterator[n]; if (o === t) return r.delegate = null, "throw" === n && e.iterator["return"] && (r.method = "return", r.arg = t, maybeInvokeDelegate(e, r), "throw" === r.method) || "return" !== n && (r.method = "throw", r.arg = new TypeError("The iterator does not provide a '" + n + "' method")), y; var i = tryCatch(o, e.iterator, r.arg); if ("throw" === i.type) return r.method = "throw", r.arg = i.arg, r.delegate = null, y; var a = i.arg; return a ? a.done ? (r[e.resultName] = a.value, r.next = e.nextLoc, "return" !== r.method && (r.method = "next", r.arg = t), r.delegate = null, y) : a : (r.method = "throw", r.arg = new TypeError("iterator result is not an object"), r.delegate = null, y); } function pushTryEntry(t) { var e = { tryLoc: t[0] }; 1 in t && (e.catchLoc = t[1]), 2 in t && (e.finallyLoc = t[2], e.afterLoc = t[3]), this.tryEntries.push(e); } function resetTryEntry(t) { var e = t.completion || {}; e.type = "normal", delete e.arg, t.completion = e; } function Context(t) { this.tryEntries = [{ tryLoc: "root" }], t.forEach(pushTryEntry, this), this.reset(!0); } function values(e) { if (e || "" === e) { var r = e[a]; if (r) return r.call(e); if ("function" == typeof e.next) return e; if (!isNaN(e.length)) { var o = -1, i = function next() { for (; ++o < e.length;) if (n.call(e, o)) return next.value = e[o], next.done = !1, next; return next.value = t, next.done = !0, next; }; return i.next = i; } } throw new TypeError(_typeof(e) + " is not iterable"); } return GeneratorFunction.prototype = GeneratorFunctionPrototype, o(g, "constructor", { value: GeneratorFunctionPrototype, configurable: !0 }), o(GeneratorFunctionPrototype, "constructor", { value: GeneratorFunction, configurable: !0 }), GeneratorFunction.displayName = define(GeneratorFunctionPrototype, u, "GeneratorFunction"), e.isGeneratorFunction = function (t) { var e = "function" == typeof t && t.constructor; return !!e && (e === GeneratorFunction || "GeneratorFunction" === (e.displayName || e.name)); }, e.mark = function (t) { return Object.setPrototypeOf ? Object.setPrototypeOf(t, GeneratorFunctionPrototype) : (t.__proto__ = GeneratorFunctionPrototype, define(t, u, "GeneratorFunction")), t.prototype = Object.create(g), t; }, e.awrap = function (t) { return { __await: t }; }, defineIteratorMethods(AsyncIterator.prototype), define(AsyncIterator.prototype, c, function () { return this; }), e.AsyncIterator = AsyncIterator, e.async = function (t, r, n, o, i) { void 0 === i && (i = Promise); var a = new AsyncIterator(wrap(t, r, n, o), i); return e.isGeneratorFunction(r) ? a : a.next().then(function (t) { return t.done ? t.value : a.next(); }); }, defineIteratorMethods(g), define(g, u, "Generator"), define(g, a, function () { return this; }), define(g, "toString", function () { return "[object Generator]"; }), e.keys = function (t) { var e = Object(t), r = []; for (var n in e) r.push(n); return r.reverse(), function next() { for (; r.length;) { var t = r.pop(); if (t in e) return next.value = t, next.done = !1, next; } return next.done = !0, next; }; }, e.values = values, Context.prototype = { constructor: Context, reset: function reset(e) { if (this.prev = 0, this.next = 0, this.sent = this._sent = t, this.done = !1, this.delegate = null, this.method = "next", this.arg = t, this.tryEntries.forEach(resetTryEntry), !e) for (var r in this) "t" === r.charAt(0) && n.call(this, r) && !isNaN(+r.slice(1)) && (this[r] = t); }, stop: function stop() { this.done = !0; var t = this.tryEntries[0].completion; if ("throw" === t.type) throw t.arg; return this.rval; }, dispatchException: function dispatchException(e) { if (this.done) throw e; var r = this; function handle(n, o) { return a.type = "throw", a.arg = e, r.next = n, o && (r.method = "next", r.arg = t), !!o; } for (var o = this.tryEntries.length - 1; o >= 0; --o) { var i = this.tryEntries[o], a = i.completion; if ("root" === i.tryLoc) return handle("end"); if (i.tryLoc <= this.prev) { var c = n.call(i, "catchLoc"), u = n.call(i, "finallyLoc"); if (c && u) { if (this.prev < i.catchLoc) return handle(i.catchLoc, !0); if (this.prev < i.finallyLoc) return handle(i.finallyLoc); } else if (c) { if (this.prev < i.catchLoc) return handle(i.catchLoc, !0); } else { if (!u) throw Error("try statement without catch or finally"); if (this.prev < i.finallyLoc) return handle(i.finallyLoc); } } } }, abrupt: function abrupt(t, e) { for (var r = this.tryEntries.length - 1; r >= 0; --r) { var o = this.tryEntries[r]; if (o.tryLoc <= this.prev && n.call(o, "finallyLoc") && this.prev < o.finallyLoc) { var i = o; break; } } i && ("break" === t || "continue" === t) && i.tryLoc <= e && e <= i.finallyLoc && (i = null); var a = i ? i.completion : {}; return a.type = t, a.arg = e, i ? (this.method = "next", this.next = i.finallyLoc, y) : this.complete(a); }, complete: function complete(t, e) { if ("throw" === t.type) throw t.arg; return "break" === t.type || "continue" === t.type ? this.next = t.arg : "return" === t.type ? (this.rval = this.arg = t.arg, this.method = "return", this.next = "end") : "normal" === t.type && e && (this.next = e), y; }, finish: function finish(t) { for (var e = this.tryEntries.length - 1; e >= 0; --e) { var r = this.tryEntries[e]; if (r.finallyLoc === t) return this.complete(r.completion, r.afterLoc), resetTryEntry(r), y; } }, "catch": function _catch(t) { for (var e = this.tryEntries.length - 1; e >= 0; --e) { var r = this.tryEntries[e]; if (r.tryLoc === t) { var n = r.completion; if ("throw" === n.type) { var o = n.arg; resetTryEntry(r); } return o; } } throw Error("illegal catch attempt"); }, delegateYield: function delegateYield(e, r, n) { return this.delegate = { iterator: values(e), resultName: r, nextLoc: n }, "next" === this.method && (this.arg = t), y; } }, e; }
 function asyncGeneratorStep(n, t, e, r, o, a, c) { try { var i = n[a](c), u = i.value; } catch (n) { return void e(n); } i.done ? t(u) : Promise.resolve(u).then(r, o); }
@@ -69,28 +69,55 @@ var getRegistroTurnoById = exports.getRegistroTurnoById = /*#__PURE__*/function 
     return _ref2.apply(this, arguments);
   };
 }();
-var createRegistroTurno = exports.createRegistroTurno = /*#__PURE__*/function () {
+var getRegistroTurnoByIdMedico = exports.getRegistroTurnoByIdMedico = /*#__PURE__*/function () {
   var _ref3 = _asyncToGenerator(/*#__PURE__*/_regeneratorRuntime().mark(function _callee3(req, res) {
-    var _req$body, num_pacientes, id_medico, id_horario, pool, result, id_rt;
+    var id_medico, pool, result;
     return _regeneratorRuntime().wrap(function _callee3$(_context3) {
       while (1) switch (_context3.prev = _context3.next) {
         case 0:
+          id_medico = req.params.id_medico;
+          _context3.next = 3;
+          return (0, _database.getConnection)();
+        case 3:
+          pool = _context3.sent;
+          _context3.next = 6;
+          return pool.request().input('Id', id_medico).query("SELECT * FROM reg_turno WHERE id_medico = @Id");
+        case 6:
+          result = _context3.sent;
+          console.log(result);
+          res.json(result.recordset);
+        case 9:
+        case "end":
+          return _context3.stop();
+      }
+    }, _callee3);
+  }));
+  return function getRegistroTurnoByIdMedico(_x5, _x6) {
+    return _ref3.apply(this, arguments);
+  };
+}();
+var createRegistroTurno = exports.createRegistroTurno = /*#__PURE__*/function () {
+  var _ref4 = _asyncToGenerator(/*#__PURE__*/_regeneratorRuntime().mark(function _callee4(req, res) {
+    var _req$body, num_pacientes, id_medico, id_horario, pool, result, id_rt;
+    return _regeneratorRuntime().wrap(function _callee4$(_context4) {
+      while (1) switch (_context4.prev = _context4.next) {
+        case 0:
           _req$body = req.body, num_pacientes = _req$body.num_pacientes, id_medico = _req$body.id_medico, id_horario = _req$body.id_horario;
           if (!(num_pacientes == null || id_medico == null || id_horario == null)) {
-            _context3.next = 3;
+            _context4.next = 3;
             break;
           }
-          return _context3.abrupt("return", res.status(400).send('Todos los campos son obligatorios'));
+          return _context4.abrupt("return", res.status(400).send('Todos los campos son obligatorios'));
         case 3:
-          _context3.prev = 3;
-          _context3.next = 6;
+          _context4.prev = 3;
+          _context4.next = 6;
           return (0, _database.getConnection)();
         case 6:
-          pool = _context3.sent;
-          _context3.next = 9;
+          pool = _context4.sent;
+          _context4.next = 9;
           return pool.request().input('Num_pacientes', _database.sql.Int, num_pacientes).input('Id_medico', _database.sql.Int, id_medico).input('Id_horario', _database.sql.Int, id_horario).query("INSERT INTO reg_turno (num_pacientes, id_medico, id_horario)" + "OUTPUT INSERTED.id_rt VALUES (@Num_pacientes, @Id_medico, @Id_horario)");
         case 9:
-          result = _context3.sent;
+          result = _context4.sent;
           id_rt = result.recordset[0].id_rt;
           console.log(num_pacientes, id_medico, id_horario);
           res.json({
@@ -98,45 +125,45 @@ var createRegistroTurno = exports.createRegistroTurno = /*#__PURE__*/function ()
             id_medico: id_medico,
             id_horario: id_horario
           });
-          _context3.next = 19;
+          _context4.next = 19;
           break;
         case 15:
-          _context3.prev = 15;
-          _context3.t0 = _context3["catch"](3);
-          console.log(_context3.t0);
+          _context4.prev = 15;
+          _context4.t0 = _context4["catch"](3);
+          console.log(_context4.t0);
           res.status(500);
         case 19:
         case "end":
-          return _context3.stop();
+          return _context4.stop();
       }
-    }, _callee3, null, [[3, 15]]);
+    }, _callee4, null, [[3, 15]]);
   }));
-  return function createRegistroTurno(_x5, _x6) {
-    return _ref3.apply(this, arguments);
+  return function createRegistroTurno(_x7, _x8) {
+    return _ref4.apply(this, arguments);
   };
 }();
 var updateRegistroTurno = exports.updateRegistroTurno = /*#__PURE__*/function () {
-  var _ref4 = _asyncToGenerator(/*#__PURE__*/_regeneratorRuntime().mark(function _callee4(req, res) {
+  var _ref5 = _asyncToGenerator(/*#__PURE__*/_regeneratorRuntime().mark(function _callee5(req, res) {
     var id_rt, _req$body2, num_pacientes, id_medico, id_horario, pool;
-    return _regeneratorRuntime().wrap(function _callee4$(_context4) {
-      while (1) switch (_context4.prev = _context4.next) {
+    return _regeneratorRuntime().wrap(function _callee5$(_context5) {
+      while (1) switch (_context5.prev = _context5.next) {
         case 0:
           id_rt = req.params.id_rt;
           _req$body2 = req.body, num_pacientes = _req$body2.num_pacientes, id_medico = _req$body2.id_medico, id_horario = _req$body2.id_horario;
           if (!(!num_pacientes || !id_medico || !id_horario)) {
-            _context4.next = 4;
+            _context5.next = 4;
             break;
           }
-          return _context4.abrupt("return", res.status(400).json({
+          return _context5.abrupt("return", res.status(400).json({
             msg: 'Error, por favor llene todos los campos'
           }));
         case 4:
-          _context4.prev = 4;
-          _context4.next = 7;
+          _context5.prev = 4;
+          _context5.next = 7;
           return (0, _database.getConnection)();
         case 7:
-          pool = _context4.sent;
-          _context4.next = 10;
+          pool = _context5.sent;
+          _context5.next = 10;
           return pool.request().input('Id_rt', _database.sql.Int, id_rt).input('Num_pacientes', _database.sql.Int, num_pacientes).input('Id_medico', _database.sql.Int, id_medico).input('Id_horario', _database.sql.Int, id_horario).query("UPDATE reg_turno SET num_pacientes = @Num_pacientes, id_medico = @Id_medico, id_horario = @Id_horario WHERE id_rt = @Id_rt");
         case 10:
           console.log(num_pacientes, id_medico, id_horario);
@@ -145,59 +172,59 @@ var updateRegistroTurno = exports.updateRegistroTurno = /*#__PURE__*/function ()
             id_medico: id_medico,
             id_horario: id_horario
           });
-          _context4.next = 18;
+          _context5.next = 18;
           break;
         case 14:
-          _context4.prev = 14;
-          _context4.t0 = _context4["catch"](4);
-          console.log(_context4.t0);
+          _context5.prev = 14;
+          _context5.t0 = _context5["catch"](4);
+          console.log(_context5.t0);
           res.status(500).json({
             msg: 'Error al actualizar el turno'
           });
         case 18:
         case "end":
-          return _context4.stop();
+          return _context5.stop();
       }
-    }, _callee4, null, [[4, 14]]);
+    }, _callee5, null, [[4, 14]]);
   }));
-  return function updateRegistroTurno(_x7, _x8) {
-    return _ref4.apply(this, arguments);
+  return function updateRegistroTurno(_x9, _x10) {
+    return _ref5.apply(this, arguments);
   };
 }();
 var deleteRegistroTurno = exports.deleteRegistroTurno = /*#__PURE__*/function () {
-  var _ref5 = _asyncToGenerator(/*#__PURE__*/_regeneratorRuntime().mark(function _callee5(req, res) {
+  var _ref6 = _asyncToGenerator(/*#__PURE__*/_regeneratorRuntime().mark(function _callee6(req, res) {
     var id_rt, pool;
-    return _regeneratorRuntime().wrap(function _callee5$(_context5) {
-      while (1) switch (_context5.prev = _context5.next) {
+    return _regeneratorRuntime().wrap(function _callee6$(_context6) {
+      while (1) switch (_context6.prev = _context6.next) {
         case 0:
           id_rt = req.params.id_rt;
-          _context5.prev = 1;
-          _context5.next = 4;
+          _context6.prev = 1;
+          _context6.next = 4;
           return (0, _database.getConnection)();
         case 4:
-          pool = _context5.sent;
-          _context5.next = 7;
+          pool = _context6.sent;
+          _context6.next = 7;
           return pool.request().input('Id', _database.sql.Int, id_rt).query("DELETE FROM reg_turno WHERE id_rt = @Id_rt");
         case 7:
           res.json({
             msg: "El turno ".concat(id_rt, " se ha eliminado correctamente")
           });
-          _context5.next = 14;
+          _context6.next = 14;
           break;
         case 10:
-          _context5.prev = 10;
-          _context5.t0 = _context5["catch"](1);
-          console.log(_context5.t0);
+          _context6.prev = 10;
+          _context6.t0 = _context6["catch"](1);
+          console.log(_context6.t0);
           res.status(500).json({
             msg: 'Error al eliminar el turno'
           });
         case 14:
         case "end":
-          return _context5.stop();
+          return _context6.stop();
       }
-    }, _callee5, null, [[1, 10]]);
+    }, _callee6, null, [[1, 10]]);
   }));
-  return function deleteRegistroTurno(_x9, _x10) {
-    return _ref5.apply(this, arguments);
+  return function deleteRegistroTurno(_x11, _x12) {
+    return _ref6.apply(this, arguments);
   };
 }();
--- build/routes/reg-turno.routes.js
+++ build/routes/reg-turno.routes.js
@@ -9,6 +9,7 @@ var _regTurno = require("../controller/reg-turno.controller");
 var router = (0, _express.Router)();
 router.get('/', _regTurno.getRegistroTurno);
 router.get('/:id_rt', _regTurno.getRegistroTurnoById);
+router.get('/medico/:id_medico', _regTurno.getRegistroTurnoByIdMedico);
 router.post('/', _regTurno.createRegistroTurno);
 router.put('/:id_rt', _regTurno.updateRegistroTurno);
 router["delete"]('/:id_rt', _regTurno.deleteRegistroTurno);
